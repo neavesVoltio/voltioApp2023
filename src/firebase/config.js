dcf2ae3e9fd4db5068f471bced8b5a08fbdd9d8f
Neaves Voltio <119772439+neavesVoltio@users.noreply.github.com>
2023-05-26 17:26:05 -0600

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -23,4 +23,4 @@
       export const app = initializeApp(firebaseConfig);
       export const analytics = getAnalytics(app);
       export const auth = getAuth(app) 
-      export const messagings = getMessaging()
+      export const messaging = getMessaging()
